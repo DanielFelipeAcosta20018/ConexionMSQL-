e30f12ae52e3da3863011f0fd61a104ef38a8f66
USUARIO <USUARIO@DESKTOP-15GPR90>
2018-09-26 21:29:29 -0500

    Se creo la clase tercero que va implementar los metodos del CRUD

---
+++ src/mysqlconexion/Tercero.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysqlconexion;
+import Terceros.*;
+import java.sql.*;
+
+
+
+public class Tercero extends TerceroPOA {
+  Conexion obConexion = new Conexion();
+
+    @Override
+    public boolean insertarTercero(String nombre, String apellido, String telefono) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean actualizarTercero(int id, String nombre, String apellido, String telefono) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarTercero(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String consultarTercero(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void shoutdown() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+  
+}
